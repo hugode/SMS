7caaf7deaa29ce7dec3e65efeeef02a66459e78c
hugode <hugo_dedaj@hotmail.com>
2018-06-03 16:14:39 +0200

    done for inserting data in marksheet_student

--- application/models/model_marksheet.php
+++ application/models/model_marksheet.php
@@ -4,6 +4,12 @@ class model_marksheet extends CI_Model
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->model('model_classes');
+		/*Looding the section model*(Ngarkimi i section model)*/
+		$this->load->model('model_section');
+		/*Loadinf Model_subject(Ngarkimi i subject model*/
+		$this->load->model('model_subject');
+		$this->load->model('model_student');
 	}
 
 
@@ -21,13 +27,36 @@ class model_marksheet extends CI_Model
 		/*Ketu kemi bere krijimin e sectionit duhke u bazuar ne class id dhe dhe me inputet te cilat jane*/
 		if($classId)
 		{
+			$sectionData=$this->model_section->fetchSectionDataByClass($classId);
 			$insert_data=array(
 				'marksheet_name'=>$this->input->post('marksheetName'),
 				'class_id'=> $classId,
 				'marksheet_date'=>$this->input->post('examDate')
 			);
-			$query=$this->db->insert('marksheet',$insert_data);
-			return($query==true)?true:false;
+			$this->db->insert('marksheet',$insert_data);
+
+			$marksheetId=$this->db->insert_id();
+			foreach ($sectionData as $key=>$value) {
+				$studentData=$this->model_student->fetchStudentDataByClassAndSection($classId,$value['section_id']);
+				$subjectData=$this->model_subject->fetchSubjectDataByClass($classId);
+				foreach ($studentData as $studentKey=>$studentValue) {
+					foreach ($subjectData as $keyS => $subjectValue) {
+						$marksheet_studentData=array(
+							'student_id'=>$studentValue['student_id'],
+							'subject_id'=>$subjectValue['subject_id'],
+						    'marksheet_id'=>$marksheetId,
+						    'class_id'=>$classId,
+						    'section_id'=>$value['section_id']
+
+					    );
+						$this->db->insert('marksheet_student',$marksheet_studentData);
+					}
+				}
+
+			}
+			return true;
+		}else{
+			return false;
 		}
 	}
 }
